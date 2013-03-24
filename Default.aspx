ed7ee0ae3fa68bb30e0292502b586b00059cc331
unknown <kstleger@EN4061192214.fulton.ad.asu.edu>
2013-03-24 14:51:00 -0700

    first

---
+++ Default.aspx
@@ -0,0 +1,49 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.master" AutoEventWireup="true"
+    EnableViewStateMac="false" CodeFile="Default.aspx.cs" EnableViewState="false" Inherits="_Default" %>
+
+<asp:Content ID="HeaderContent" runat="server" ContentPlaceHolderID="HeadContent">
+</asp:Content>
+<asp:Content ID="BodyContent" runat="server" ContentPlaceHolderID="MainContent">
+    <h2>
+        KyleService TryIt Test Page
+    </h2>
+    <p>
+        Please enter a URL for the Top10Words service. <asp:TextBox ID="s1Input" runat="server" Width="200px">Copy URL here</asp:TextBox> &nbsp; 
+        <asp:Button ID="s1Submit" runat="server" OnClick="s1Submit_Click" Text="Submit" />    </p>
+    <p>
+        Here are the results:
+        <asp:Label ID="s1Page" runat="server"></asp:Label>
+    </p>
+
+    <p> Please enter a URL for the WordFilter service. <asp:TextBox ID="s2Input" runat="server" Width="200px">Copy URL here</asp:TextBox> &nbsp;
+        <asp:Button ID="s2Submit" runat="server" OnClick="s2Submit_Click" Text="Submit" />    </p>
+
+    <p>
+        Here are the results:
+        <asp:Label ID="s2Page" runat="server"></asp:Label>
+    </p>
+
+    <p> Please enter a URL for the getWsdlAddress <asp:TextBox ID="s3Input" runat="server" Width="200px">Copy URL here</asp:TextBox> &nbsp;
+        <asp:Button ID="s3Submit" runat="server" OnClick="s3Submit_Click" Text="Submit" />   </p>
+
+    <p>
+        Here are the results:
+        <asp:Label ID="s3Page" runat="server"></asp:Label>
+    </p>
+
+
+    <p> Please enter a URL to shorten. <asp:TextBox ID="s4Input" runat="server" Width="200px">Copy URL here</asp:TextBox> &nbsp;
+        <asp:Button ID="s4Submit" runat="server" OnClick="s4Submit_Click" Text="Submit" />  </p>
+    
+    <p> Here are the results:
+        <asp:Label ID="s4Page" runat="server"></asp:Label>
+    </p>
+
+    <p> Please enter a zipcode and a store name. <asp:TextBox ID="s5Input1" runat="server" Width="200px">Zipcode here</asp:TextBox> &nbsp <asp:TextBox ID="s5Input2" runat="server" Width="200px">Store Name here</asp:TextBox>&nbsp;
+        <asp:Button ID="s5Submit" runat="server" OnClick="s5Submit_Click" Text="Submit" /> </p>
+    
+    <p> Here are the results:
+        <asp:Label ID="s5Page" runat="server"></asp:Label>
+    </p>
+
+</asp:Content>
---
+++ Default.aspx.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void s1Submit_Click(object sender, EventArgs e)
+    {
+        string[] result;
+
+        KyleService.ServiceClient myclient = new KyleService.ServiceClient();
+        try
+        {
+            result = myclient.Top10Words(s1Input.Text);
+            for (int i = 0; i < result.Length; i++)
+            {
+                s1Page.Text += result[i] + "<br>";
+            }
+
+            
+        }
+        catch (Exception ec)
+        {
+            s1Page.Text = ec.Message.ToString();
+        }
+    }
+
+    protected void s2Submit_Click(object sender, EventArgs e)
+    {
+        KyleService.ServiceClient myclient = new KyleService.ServiceClient();
+
+        try
+        {
+            s2Page.Text = myclient.WordFilter(s2Input.Text);
+        }
+        catch (Exception ec)
+        {
+            s2Page.Text = ec.Message.ToString();
+        }
+
+    }
+
+    protected void s3Submit_Click(object sender, EventArgs e)
+    {
+        string[] result;
+        KyleService.ServiceClient myclient = new KyleService.ServiceClient();
+        try
+        {
+            result = myclient.getWsdlAddress(s3Input.Text);
+            for (int i = 0; i < result.Length; i++)
+            {
+                s3Page.Text += result[i] + "<br>";
+            }
+
+        }
+        catch (Exception ec)
+        {
+            s3Page.Text = ec.Message.ToString();
+        }
+    }
+
+    protected void s4Submit_Click(object sender, EventArgs e)
+    {
+        KyleService.ServiceClient myclient = new KyleService.ServiceClient();
+
+        try
+        {
+            s4Page.Text = myclient.makeShortUrl(s4Input.Text);
+        }
+        catch (Exception ec)
+        {
+            s4Page.Text = ec.Message.ToString();
+        }
+    }
+
+    protected void s5Submit_Click(object sender, EventArgs e)
+    {
+        KyleService.ServiceClient myclient = new KyleService.ServiceClient();
+
+        try
+        {
+            s5Page.Text = myclient.findNearestStore(s5Input1.Text, s5Input2.Text);
+        }
+        catch (Exception ec)
+        {
+            s5Page.Text = ec.Message.ToString();
+        }
+    }
+}
